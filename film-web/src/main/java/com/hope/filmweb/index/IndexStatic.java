306753f545eba39bb9863fd86497cb99a948388a
Administrator <1056484792@qq.com>
2018-10-10 10:07:04 +0800

    a

--- film-web/src/main/java/com/hope/filmweb/index/IndexStatic.java
+++ film-web/src/main/java/com/hope/filmweb/index/IndexStatic.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -21,26 +23,31 @@ import java.util.Map;
  * @Description:
  */
 @Controller
-public class IndexStatic {
+public class IndexStatic extends HttpServlet {
 
     @Resource
     Configuration cfg;
 
     @RequestMapping("/static")
-    public String main(Model model) {
+    public String main(Model model,HttpServletRequest request) {
         String w = "Welcome FreeMarker!";
         Map root = new HashMap(16);
         root.put("w", w);
-        freeMarkerContent(root);
+        freeMarkerContent(root,request);
         model.addAttribute("w", "Welcome FreeMarker!");
         return "freemaker";
     }
 
-    private void freeMarkerContent(Map<String, Object> root) {
+    private void freeMarkerContent(Map<String, Object> root,HttpServletRequest request) {
         try {
+
+            String path = System.getProperty("user.dir");
+            String allPath = path + "\\film-web\\src\\main\\resources\\html\\index.html";
+            System.out.println("allPath   "+allPath);
+
             Template temp = cfg.getTemplate("freemaker.ftl");
             //以classpath下面的static目录作为静态页面的存储目录，同时命名生成的静态html文件名称
-            Writer file = new FileWriter(new File("E:\\工作文档\\film-project\\film-web\\src\\main\\resources\\html\\index.html"));
+            Writer file = new FileWriter(new File(allPath));
             temp.process(root, file);
             file.flush();
             file.close();
