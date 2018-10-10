1f3c408645b8c197b662fb99c846e131e7df93ad
Administrator <1056484792@qq.com>
2018-10-10 17:48:37 +0800

    路径提交

--- film-web/src/main/java/com/hope/filmweb/index/IndexStatic.java
+++ film-web/src/main/java/com/hope/filmweb/index/IndexStatic.java
@@ -44,6 +44,7 @@ public class IndexStatic extends HttpServlet {
             String allPath = path + "\\film-web\\src\\main\\resources\\html\\index.html";
             System.out.println("allPath   "+allPath);
 
+
             Template temp = cfg.getTemplate("freemaker.ftl");
             //以classpath下面的static目录作为静态页面的存储目录，同时命名生成的静态html文件名称
             Writer file = new FileWriter(new File(allPath));
