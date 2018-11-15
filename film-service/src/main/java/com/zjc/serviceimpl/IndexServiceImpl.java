c250b4c468bc4d7c5997c2dc4fa188397661fbe3
zhangjc <zhangjc@tongc-sorft.com>
2018-11-15 17:43:15 +0800

    提交service demo

--- film-service/src/main/java/com/zjc/serviceimpl/IndexServiceImpl.java
+++ film-service/src/main/java/com/zjc/serviceimpl/IndexServiceImpl.java
@@ -1,12 +1,14 @@
 package com.zjc.serviceimpl;
 
 import com.zjc.index.IndexService;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author: zhangjiachen
  * @Date: 2018/11/15 17:38
  * @Description:
  */
+@Service
 public class IndexServiceImpl implements IndexService {
     @Override
     public void showIndexData() {
