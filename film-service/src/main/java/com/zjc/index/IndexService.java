c4fc76339c223ad0bce78406aa556e2724142cec
zhangjc <zhangjc@tongc-sorft.com>
2018-11-15 17:41:18 +0800

    增加service demo

---
+++ film-service/src/main/java/com/zjc/index/IndexService.java
@@ -0,0 +1,14 @@
+package com.zjc.index;
+
+/**
+ * @Author: zhangjiachen
+ * @Date: 2018/11/15 17:38
+ * @Description:
+ */
+public interface IndexService {
+
+    /**
+     * 展示首页数据
+     */
+    public void showIndexData();
+}
---
+++ film-service/src/main/java/com/zjc/serviceimpl/IndexServiceImpl.java
@@ -0,0 +1,15 @@
+package com.zjc.serviceimpl;
+
+import com.zjc.index.IndexService;
+
+/**
+ * @Author: zhangjiachen
+ * @Date: 2018/11/15 17:38
+ * @Description:
+ */
+public class IndexServiceImpl implements IndexService {
+    @Override
+    public void showIndexData() {
+
+    }
+}
