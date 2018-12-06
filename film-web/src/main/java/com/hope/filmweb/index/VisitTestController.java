76202838b87e395bbd8c9e6514a00b650167d4a0
zhangjc <zhangjc@tongc-sorft.com>
2018-12-06 11:14:10 +0800

    增加日志

--- film-web/src/main/java/com/hope/filmweb/index/VisitTestController.java
+++ film-web/src/main/java/com/hope/filmweb/index/VisitTestController.java
@@ -1,6 +1,8 @@
 package com.hope.filmweb.index;
 
 import com.zjc.index.IndexService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +11,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author:Zhang jc
  * @date: 2018/10/9 16:52
  * @description:
  */
 @Controller
+@Slf4j
 public class VisitTestController {
 
     @Autowired
     private IndexService indexService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        log.info("请求进来了，请求ip为：{},名称为：{},端口为:{}", request.getRemoteAddr(),
+                request.getRemoteHost(), request.getRemotePort());
         return "html/index";
     }
 
