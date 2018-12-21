75cba66a1b4fa6847b8fd47e80b2403d83bf5d50
zhangjc <zhangjc@tongc-sorft.com>
2018-12-21 18:24:18 +0800

    jedis

--- film-web/src/main/java/com/hope/filmweb/config/RedisConfig.java
+++ film-web/src/main/java/com/hope/filmweb/config/RedisConfig.java
@@ -1,11 +1,13 @@
 package com.hope.filmweb.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.Jedis;
 
 import java.io.Serializable;
 
@@ -16,6 +18,7 @@ import java.io.Serializable;
  */
 @Configuration
 @EnableCaching
+@Slf4j
 public class RedisConfig {
     @Autowired
     private JedisConnectionFactory connectionFactory;
@@ -23,8 +26,20 @@ public class RedisConfig {
     @Bean(name = "redisTemplate")
     public RedisTemplate<Serializable, Object> getTemplate() {
         RedisTemplate<Serializable, Object> redisTemplate = new RedisTemplate();
+        log.info("开始打印连接{},port{}", connectionFactory.getClientName(), connectionFactory.getPort());
+        log.info("host{}", connectionFactory.getClientName());
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
+
+    @Bean(name = "jedis")
+    public Jedis getJedis() {
+        Jedis jedis = new Jedis("47.100.237.222", 6379);
+        jedis.set("zjc", "张嘉琛");
+        String zjc = jedis.get("zjc");
+        log.info("zjc::::::::::::::::{}", zjc);
+        return jedis;
+    }
+
 
 }
