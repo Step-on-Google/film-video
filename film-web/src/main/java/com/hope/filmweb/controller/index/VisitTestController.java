e7bb8058dff470f007add8a1933db58228bcca19
zhangjc <zhangjc@tongc-sorft.com>
2019-01-14 16:34:43 +0800

    修改kafka版本

--- film-web/src/main/java/com/hope/filmweb/controller/index/VisitTestController.java
+++ film-web/src/main/java/com/hope/filmweb/controller/index/VisitTestController.java
@@ -75,26 +75,10 @@ public class VisitTestController {
     @ResponseBody
     public String testKafka() {
         try {
-//            Map<String, String> map = new HashMap(16);
-//            map.put("test", "zjc");
-//            ListenableFuture zjcTopic = kafkaTemplate.send("zjcTopic", "1", "你好呀!");
-
-            Properties props = new Properties();
-            props.put("bootstrap.servers", "47.100.237.222:9092");
-            props.put("acks", "all");
-            props.put("retries", 0);
-            props.put("batch.size", 16384);
-            props.put("linger.ms", 1);
-            props.put("buffer.memory", 33554432);
-            props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-            Producer<String, String> producer = new KafkaProducer(props);
-            for(int i = 0; i < 100; i++)
-                producer.send(new ProducerRecord<String, String>("zjcTopic", Integer.toString(i), Integer.toString(i)));
-
-            producer.close();
-//            log.info("打印kafka返回{}", zjcTopic);
+            Map<String, String> map = new HashMap(16);
+            map.put("test", "zjc");
+            ListenableFuture zjcTopic = kafkaTemplate.send("zjcTopic", "1", "你好呀!");
+            log.info("打印kafka返回{}", zjcTopic);
         } catch (Exception e) {
             log.error("kafka异常!", e);
             return "kafkaTest error";
