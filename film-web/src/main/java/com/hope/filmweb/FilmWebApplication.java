d0c638a0aba88dd3a97ab0509eb430cd6360ea10
zhangjc <zhangjc@tongc-sorft.com>
2019-01-28 13:42:59 +0800

    修复springCloud启动失败原因 为版本问题

--- film-web/src/main/java/com/hope/filmweb/FilmWebApplication.java
+++ film-web/src/main/java/com/hope/filmweb/FilmWebApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
